06311350cfbe19e226bd1c3f440db4a855df080c
bibig <achaochao@gmail.com>
2013-01-19 16:48:10 +0800

    redirect www domain to non-www domain

--- app.js
+++ app.js
@@ -115,6 +115,15 @@ app.param('pid', load.page);
 app.param('_pid', validate.ifNodeIsFrozen);
 app.param('_pid', load.newPage);
 
+
+app.get('/*', function(req, res, next) {
+	if (req.subdomains.count === 1 && req.subdomains[0] === 'www') {
+		res.redirect(req.protocol + '//' + req.host.replace(/^www\./, '') + req.path, 301);
+	} else { 
+		next();
+	}
+});
+
 app.all('*', lang.autoset);
 
 // site default node
